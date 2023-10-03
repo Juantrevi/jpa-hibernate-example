680224b13f305daa7ba1a58923758e0c7b993375
Juantrevi <juantrevi70@gmail.com>
2023-10-03 21:01:17 +0100

    CommandLineRunner - Dynamis

--- src/main/java/com/jpahibernate/ex/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/jpahibernate/ex/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.jpahibernate.ex.course.jdbc;
 
+import com.jpahibernate.ex.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,6 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(10001L, "Spring", "Ranga"));
     }
 }
