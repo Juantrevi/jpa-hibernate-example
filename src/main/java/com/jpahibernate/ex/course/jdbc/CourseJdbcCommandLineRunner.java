5aa67b98165dde748b5ccf60ff7160e8cf12c9eb
Juantrevi <juantrevi70@gmail.com>
2023-10-03 21:15:11 +0100

    CommandLineRunner - Dynamic ALL JDBC

--- src/main/java/com/jpahibernate/ex/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/jpahibernate/ex/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -14,5 +14,10 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         repository.insert(new Course(10001L, "Spring", "Ranga"));
+        repository.insert(new Course(10002L, "Spring Boot", "Ranga"));
+        repository.insert(new Course(10003L, "Spring MVC", "Ranga"));
+        repository.insert(new Course(10004L, "Spring Rest", "Ranga"));
+        repository.deleteById(10004L);
+        System.out.println(repository.findById(10001L));
     }
 }
--- src/main/java/com/jpahibernate/ex/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/jpahibernate/ex/course/jdbc/CourseJdbcRepository.java
@@ -2,6 +2,7 @@ package com.jpahibernate.ex.course.jdbc;
 
 import com.jpahibernate.ex.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -11,11 +12,17 @@ public class CourseJdbcRepository {
     @Autowired
     private JdbcTemplate springJdbcTemplate;
 
-    private static String INSERT_QUERY = """
-            insert into course (id, name, author)
-            values (?, ?, ?);
-            """;
+    private static String INSERT_QUERY = "insert into course (id, name, author) values (?, ?, ?)";
+    private static String DELETE_QUERY = "delete from course where id = ?";
+    private static String SELECT_QUERY = "select * from course where id = ?";
+
     public void insert(Course course){
         springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+    public void deleteById(Long id){
+        springJdbcTemplate.update(DELETE_QUERY, id);
+    }
+    public Course findById(Long id){
+        return springJdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(Course.class), id);
     }
 }
