d24403d99081103573048d0317359c164359eab7
Juantrevi <juantrevi70@gmail.com>
2023-10-03 19:16:23 +0100

    Initial commit

---
+++ src/test/java/com/jpahibernate/ex/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpahibernate.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
