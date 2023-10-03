741932fe20082a50d4d597e916f752fa308bd202
Juantrevi <juantrevi70@gmail.com>
2023-10-03 19:43:30 +0100

    CommandLineRunner

--- src/main/java/com/jpahibernate/ex/JpaHibernateApplication.java
+++ src/main/java/com/jpahibernate/ex/JpaHibernateApplication.java
@@ -8,7 +8,7 @@ public class JpaHibernateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaHibernateApplication.class, args);
-        System.out.println("Hello World!");
+
     }
 
 }
